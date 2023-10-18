1048a7d3269b5f9b9b7a40a107b404570930dc1f
Tiniuss <pertfr@ulrik.uio.no>
2023-10-18 23:11:02 +0200

    confirmation for user when dispensing

--- dhis2app/src/Insert.js
+++ dhis2app/src/Insert.js
@@ -11,6 +11,13 @@ import {
   TableHead,
   TableRow,
   TableRowHead,
+  Modal,
+  ModalTitle,
+  ModalContent,
+  ModalActions,
+  ButtonStrip,
+  Button,
+  AlertBar,
 } from '@dhis2/ui'
 
 
@@ -39,46 +46,67 @@ const formattedDate = `${year}${month}`;
 
 export function Insert({ orgUnit, mergedDataInput }) {
   const endBalanceCategory = "J2Qf1jtZuj8";
-
-
- 
+  const consumptionCategory = "rQLFnNXXIL0";
 
   const [mutate] = useDataMutation(dataMutationQuery);
 
   const [dispenseInputValues, setDispenseInputValues] = useState({});
-  const [addStockInputValues, setAddStockInputValues] = useState({});
   const [mergedData, setMergedData] = useState(mergedDataInput);
+  const [confirmationWindow, setConfirmationWindow] = useState(false);
+  const [alert, setAlert] = useState(false);
+  const [stockOut, setStockOut] = useState(false);
 
-  const onSubmit = () => {
+
+  const confirm = () => {
     if (mergedDataInput) {
+      let empty = false;
         const updatedMergedData = mergedData.map(item => {
           const dispenseInput = dispenseInputValues[item.id];
-          const addStockInput = addStockInputValues[item.id];
       
           const originalValue = item.endBalance;
           const dispenseValue = parseInt(dispenseInput) || 0;
-          const addStockValue = parseInt(addStockInput) || 0;
-          const newValue = originalValue - dispenseValue + addStockValue;
-          item.endBalance = newValue; 
-          // Update the item so the user sees the updated value
-          return item; // Assuming "endBalance" is the property to be updated
+          const newValue = originalValue - dispenseValue
           
+          if (dispenseValue > originalValue){
+            empty = true;
+          }
+
+          //only mutate if the value is changed
+          if (Number(originalValue) !== Number(newValue) && !empty){
+            item.endBalance = newValue;
+            item.consumption = Number(item.consumption) + Number(dispenseValue); 
+            console.log("MUTATE SKJER DA")
+            //update dispense value
+            mutate({
+              value: item.endBalance,
+              dataElement: item.id,
+              period: formattedDate,
+              orgUnit,
+              categoryOptionCombo: endBalanceCategory,
+            });
+            //update consumption value
+            mutate({
+              value: item.consumption,
+              dataElement: item.id,
+              period: formattedDate,
+              orgUnit,
+              categoryOptionCombo: consumptionCategory,
+            });
+          }
+          return item;
         });
-      
-  
-      setMergedData(updatedMergedData);
-  
-      // mutation to update the actual mergedDataInput
-      updatedMergedData.forEach(item => {
-        mutate({
-          value: item.endBalance, // Assuming "endBalance" is the property to be used here
-          dataElement: item.id,
-          period: formattedDate,
-          orgUnit,
-          categoryOptionCombo: endBalanceCategory,
-        });
-      });
+        if (empty){
+          console.log("NOT ENOUGHT STOCK")
+          setAlert(true);
+          setStockOut(true)
+        }
+        else{
+          console.log("plz dont skje")
+          setMergedData(updatedMergedData);
+        }
+
     }
+    setConfirmationWindow(false);
   };
   
 
@@ -88,11 +116,22 @@ export function Insert({ orgUnit, mergedDataInput }) {
     setDispenseInputValues(updatedInputValues);
   };
 
-  const handleAddStockChange = (e, id) => {
-    const updatedInputValues = { ...addStockInputValues };
-    updatedInputValues[id] = e.target.value;
-    setAddStockInputValues(updatedInputValues);
-  };
+  function showConfirmationWindow(){
+    setConfirmationWindow(true);
+  }
+
+  function decline(){
+    setConfirmationWindow(false);
+  }
+
+  function cancel(){
+    setAlert(false)
+  }
+
+  function stock(){
+    setStockOut(false)
+  }
+  
 
   return (
     <div>
@@ -101,8 +140,8 @@ export function Insert({ orgUnit, mergedDataInput }) {
           <TableRowHead>
             <TableCellHead>Commodities</TableCellHead>
             <TableCellHead>End Balance</TableCellHead>
+            <TableCellHead>Consumption</TableCellHead>
             <TableCellHead>Dispense</TableCellHead>
-            <TableCellHead>Add Stock</TableCellHead>
           </TableRowHead>
         </TableHead>
         <TableBody>
@@ -111,28 +150,99 @@ export function Insert({ orgUnit, mergedDataInput }) {
             <TableRow key={item.id}>
               <TableCell>{item.displayName}</TableCell>
               <TableCell>{item.endBalance === null ? 'Needs Update' : item.endBalance}</TableCell>
+              <TableCell>{item.consumption === null ? 'Needs Update' : item.consumption}</TableCell>
               <TableCell>
                   <input
                     name="dispense"
-                    type="text"
+                    type="number"
+                    min="0"
                     onChange={e => handleDispenseChange(e, item.id)}
                   />
-                </TableCell>
-                <TableCell>
-                  <input
-                    name="add"
-                    type="text"
-                    onChange={e => handleAddStockChange(e, item.id)}
-                  />
-                </TableCell>
-             
+                </TableCell>             
             </TableRow>
           ))
         }
           
         </TableBody>
       </Table>
-      <button onClick={onSubmit}>Update</button>
+      <button onClick={showConfirmationWindow}>Update</button>
+      {confirmationWindow && (
+        <Modal>
+          <ModalTitle>Confirm Dispense:</ModalTitle>
+          <ModalContent>
+              <p>Commodities about to be dispensed:</p>
+              <ul>
+                {mergedDataInput.map((item) => {
+                  const dispenseValue = parseInt(dispenseInputValues[item.id]) || 0;
+                  if (dispenseValue > 0) {
+                    return (
+                      <li key={item.id}>
+                        {item.displayName} - Dispense: {dispenseValue}
+                      </li>
+                    );
+                  }
+                  return null;
+                })}
+              </ul>
+          </ModalContent>
+          <ModalActions>
+            <ButtonStrip end>
+              <Button secondary onClick={decline}>
+                Decline
+              </Button>
+              <Button primary onClick={confirm}>
+                Confirm
+              </Button>
+            </ButtonStrip>
+          </ModalActions>
+        </Modal>
+      )}
+      {stockOut && (
+        <Modal>
+          <ModalTitle>Not enoguht stock</ModalTitle>
+          <ModalContent>
+              <ul>
+                {mergedDataInput.map((item) => {
+                  const dispenseValue = parseInt(dispenseInputValues[item.id]) || 0;
+                  if (dispenseValue > item.endBalance) {
+                    return (
+                      <li key={item.id}>
+                        {item.displayName}: {item.endBalance} tryed to Dispense: {dispenseValue}
+                      </li>
+                    );
+                  }
+                  return null;
+                })}
+              </ul>
+                Wait until the 14 for restock
+          </ModalContent>
+          <ModalActions>
+            <ButtonStrip end>
+              <Button secondary onClick={stock}>
+                Understand
+              </Button>
+            </ButtonStrip>
+          </ModalActions>
+        </Modal>
+      )}
+      {alert && (
+        <AlertBar 
+          actions={[
+              { 
+                //TODO: make enarby clinics and redirect to this page, when pressed
+                  label: 'check nearby clinics',
+                  onClick:  cancel 
+              },
+              {
+                  label: 'Cancel',
+                  onClick:  cancel 
+              }
+          ]}
+          permanent critical
+        >
+        Not enought stock
+      </AlertBar>
+      )}
     </div>
   );
 }
