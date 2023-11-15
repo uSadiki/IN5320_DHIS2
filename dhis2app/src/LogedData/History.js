03c470147f214555d59cc2bbf7079950361de2ee
Uvejs Sadiki <uvejss@ulrik.uio.no>
2023-11-15 13:16:45 +0100

    Fixed history, and some namechanges

--- dhis2app/src/LogedData/History.js
+++ dhis2app/src/LogedData/History.js
@@ -1,6 +1,15 @@
 import React, { useState } from 'react';
 import { getData } from '../DataStoreUtils/DatastorePull';
-import { CircularLoader, Table, TableHead, TableRowHead, TableCellHead, TableBody, TableRow, TableCell, SwitchField,ButtonStrip,Button } from '@dhis2/ui';
+import '../CSS/App.css'
+import { CircularLoader, 
+  Table, 
+  TableHead, 
+  TableRowHead, 
+  TableCellHead, 
+  TableBody, 
+  TableRow, 
+  TableCell, 
+  SegmentedControl} from '@dhis2/ui';
 
 //Function to show transaction history and recount history
 export function History() {
@@ -8,78 +17,67 @@ export function History() {
   const [showTransactions, setShowTransactions] = useState(true);
   const [dateFilter, setDateFilter] = useState(""); // Initial filter value
   
-  const handleDateFilterChange = (event) => {
-    setDateFilter(event.target.value);
-  }
-  
-
+  const handleDateFilterChange = (event) => setDateFilter(event.target.value);
 
   //Get data for both transaction and recounts
   const transactionsData = getData("Transactions");
   const recountsData = getData("Recounts");
 
-  if (transactionsData == null || recountsData == null) {
+  if (transactionsData === null || recountsData === null) {
     return <CircularLoader />;
   }
 
-  const onSwitchChange = () => {
-    setShowTransactions(!showTransactions);
-  };
-
-
-
-
   const data = showTransactions ? transactionsData : recountsData;
 
-
-  const filteredData = Object.entries(data)
-  .filter(([, item]) => {
-    let date, time;
-
-    if (showTransactions) {
-      [date, time] = item.date.split(', ');
-    } else {
-      [date, time] = item.Date.split(', ');
-    }
-    const [day, month, year] = date.split('/');
-    const formattedItemDate = `${year}-${month}-${day}`;
-
-    // Check if showTransaction is true and the date matches the filter
-    return  (!dateFilter || formattedItemDate === dateFilter);
-  });
-
+  let filteredData = [];
   
+  if (data) {
+    filteredData = Object.entries(data).filter(
+      ([, item]) => {
+        let date, time;
+
+        if (showTransactions) {
+          [date, time] = item.date.split(', ');
+        } else {
+          [date, time] = item.Date.split(', ');
+        }
+
+        const [day, month, year] = date.split('/');
+        const formattedItemDate = `${year}-${month}-${day}`;
+
+        // Check if showTransaction is true and the date matches the filter
+        return  (!dateFilter || formattedItemDate === dateFilter);
+      }
+    );
+  }
 
   const headers = showTransactions
     ? ['Date', 'Commodities', 'Values', 'Dispensed By', 'Dispensed To']
     : ['Date', 'Commodities', 'Changed By', 'From Value', 'To Value'];
 
-    return (
-      
+  return (
       <div>
-          
-        
         <h1>{showTransactions ? 'Transactions' : 'Recounts'} History</h1>
 
+         <SegmentedControl 
+            selected={showTransactions ? 'transactions' : 'recounts'}
+            onChange={({value}) => setShowTransactions(value==='transactions')}
+            options={[
+                { value: 'transactions', label: 'Transactions' },
+                { value: 'recounts', label: 'Recounts' },
+              ]}
+          />
 
         Filter By date <input type="date" value={dateFilter} onChange={handleDateFilterChange} />
 
 
-        <SwitchField 
-          checked={showTransactions}
-          onChange={onSwitchChange}
-          label={`Show ${showTransactions ? 'Transactions' : 'Recounts'}`}
-        />
-
         <Table>
           <TableHead>
-
             <TableRowHead>
               {headers.map((header) => (
                 <TableCellHead key={header}>{header}</TableCellHead>
               ))}
             </TableRowHead>
-
           </TableHead>
           
           <TableBody>
@@ -87,10 +85,8 @@ export function History() {
               <React.Fragment key={id}>
                 {item && (
                   <>
-
                     {showTransactions ? (
                       item.Commodities?.map((commodity, index) => (
-                        
                         <TableRow key={`${id}-${index}`}>
                           {index === 0 ? (
                             <TableCell rowSpan={`${item.Commodities.length}`}>{item.date}</TableCell>
@@ -105,13 +101,12 @@ export function History() {
                     : (
                       <>
                         {item.Commodities && 
-                          Object.entries(item.Commodities).map(([commodityId, commodity], index) => (
-                            <TableRow key={`${id}-${commodityId}`}>
+                          Object.entries(item.Commodities).map(([commodityName, commodity], index) => (
+                            <TableRow key={`${id}-${commodityName}`}>
                               {index === 0 ? (
                                 <TableCell rowSpan={String(Object.entries(item.Commodities).length)}>{item.Date}</TableCell>
-
                               ) : null}
-                              <TableCell>{commodityId}</TableCell>
+                              <TableCell>{commodityName}</TableCell>
                               <TableCell>{item.ChangedBy}</TableCell>
                               <TableCell>{commodity.fromValue}</TableCell>
                               <TableCell>{commodity.toValue}</TableCell>
@@ -125,7 +120,6 @@ export function History() {
             ))}
           </TableBody>
         </Table>
-       
       </div>
-    );
-  }
+  );
+}
