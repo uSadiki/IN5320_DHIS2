2dc10a879069fc68f0a9f84e70751e319aa107cc
Uvejs Sadiki <uvejss@ulrik.uio.no>
2023-11-17 11:12:24 +0100

    added icons to navigation

--- dhis2app/src/Navigation.js
+++ dhis2app/src/Navigation.js
@@ -1,14 +1,22 @@
 import React from "react";
 import { Menu, MenuItem } from "@dhis2/ui";
+import Dashboard from "./images/dashboard.png"
+import Dispense from "./images/dispense.png"
+import Nearby from "./images/nearby.png"
+import History from "./images/history.png"
+import Recount from "./images/recount.png"
+
+
+
 
 export function Navigation(props) {
   const menuItems = [
-    { label: "Dashboard", page: "Dashboard" },
-    { label: "Dispense/Stock up", page: "UpdateData" },
-    { label: "Nearby Units", page: "NearbyUnits" },
-    { label: "History", page: "History" },
-    { label: "Stock Recount", page: "StockRecount" },
-    { label: "Stock Correction", page: "DataCorrection" }
+    { label: "Dashboard", page: "Dashboard", pic: Dashboard },
+    { label: "Dispense/Stock up", page: "UpdateData", pic: Dispense },
+    { label: "Nearby Units", page: "NearbyUnits", pic: Nearby },
+    { label: "History", page: "History", pic: History },
+    { label: "Stock Recount", page: "StockRecount", pic: Recount },
+    { label: "Stock Correction", page: "DataCorrection", pic: Recount}
   ];
 
   const handleClick = page => {
@@ -18,12 +26,14 @@ export function Navigation(props) {
   return (
     <Menu>
       {menuItems.map(item => (
-        <MenuItem
-          key={item.page}
-          label={item.label}
-          active={props.activePage === item.page}
-          onClick={() => handleClick(item.page)}
-        />
+        <div className="menu" onClick={() => handleClick(item.page)}>
+            <MenuItem
+              key={item.page}
+              label={item.label}
+              active={props.activePage === item.page}
+              icon = {<img src= {item.pic} alt={item.label} className="navPic"/>}
+            />
+        </div>
       ))}
     </Menu>
   );
