d3553101d279d9800217d94cf5ed78cd610dae74
Uvejs Sadiki <uvejss@ulrik.uio.no>
2023-11-17 12:37:35 +0100

    Fixed div box in dispense

--- dhis2app/src/Dashboard/ComponentsDashboard/CommoditiesTable.js
+++ dhis2app/src/Dashboard/ComponentsDashboard/CommoditiesTable.js
@@ -14,7 +14,6 @@ const CommoditiesTable = ({ filteredCommodities }) => {
           <TableCellHead>ID</TableCellHead>
         </TableRowHead>
       </TableHead>
-
       <TableBody>
         {filteredCommodities.map((item) => (
           <TableRow key={item.id}>
